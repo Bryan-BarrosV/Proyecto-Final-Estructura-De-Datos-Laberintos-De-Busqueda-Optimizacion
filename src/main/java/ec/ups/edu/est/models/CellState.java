17032c4a6da96b937d9f61a2b8c37d05aa8090e0
KeyraCarvajal <tucorreo@gmail.com>
2025-07-27 18:53:03 -0500

    feat(ups-003): Implementing the CellState class in the model package

---
+++ src/main/java/ec/ups/edu/est/models/CellState.java
@@ -0,0 +1,11 @@
+package ec.ups.edu.est.models;
+
+public enum CellState {
+    EMPTY,    // Celda vacía
+    WALL,     // Obstáculo
+    START,    // Punto A
+    END,      // Punto B
+    PATH      // Ruta óptima (usada para pintar la solución)
+
+
+}
