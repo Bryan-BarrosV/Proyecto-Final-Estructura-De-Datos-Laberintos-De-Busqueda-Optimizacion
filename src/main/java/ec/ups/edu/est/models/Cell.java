b11f1962efe87ba167b8abbec8ce47fd3284a5a8
Keyra Carvajal <keyracarvajal26@gmail.com>
2025-07-27 19:30:22 -0500

    feat(ups-012): Implementing the Cell class in the model package

--- src/main/java/ec/ups/edu/est/models/Cell.java
+++ src/main/java/ec/ups/edu/est/models/Cell.java
@@ -1,4 +1,41 @@
 package ec.ups.edu.est.models;
 
 public class Cell {
+    private int fila;
+    private int columna;
+    private CellState estado;
+
+    public Cell(int fila, int columna, CellState estado) {
+        this.fila = fila;
+        this.columna = columna;
+        this.estado = estado;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public CellState getEstado() {
+        return estado;
+    }
+
+    public void setEstado(CellState estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Cell)) return false;
+        Cell otra = (Cell) obj;
+        return this.fila == otra.fila && this.columna == otra.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return fila * 31 + columna;
+    }
 }
