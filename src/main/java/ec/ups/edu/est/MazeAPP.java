6981addb002005b6f2b6b614588db32752a214f5
Bryan <bryanbarrosvilla@gmail.com>
2025-07-28 18:34:34 -0500

    feat(ups-025):FIX MazeAPP

--- src/main/java/ec/ups/edu/est/MazeAPP.java
+++ src/main/java/ec/ups/edu/est/MazeAPP.java
@@ -2,16 +2,16 @@ package ec.ups.edu.est;
 
 import javax.swing.*;
 
+import ec.ups.edu.est.controllers.MazeController;
 import ec.ups.edu.est.views.MazeFrame;
+
 
 public class MazeAPP {
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        SwingUtilities.invokeLater(MazeFrame::new);
+        SwingUtilities.invokeLater(() -> {
+            MazeFrame vista = new MazeFrame();
+            new MazeController(vista);
+        });
     }
 }
+
