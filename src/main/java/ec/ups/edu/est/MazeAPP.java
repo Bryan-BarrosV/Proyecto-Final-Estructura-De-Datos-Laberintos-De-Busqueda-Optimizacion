563f04dea9eed9c83bf7f7d798797d742f6486a1
Keyra Carvajal <keyracarvajal26@gmail.com>
2025-07-28 22:47:47 -0500

    feat(ups-031): MazeApp class documentation

--- src/main/java/ec/ups/edu/est/MazeAPP.java
+++ src/main/java/ec/ups/edu/est/MazeAPP.java
@@ -5,8 +5,29 @@ import javax.swing.*;
 import ec.ups.edu.est.controllers.MazeController;
 import ec.ups.edu.est.views.MazeFrame;
 
-
+/**
+ * Clase principal de la aplicación del laberinto.
+ *
+ * <p>
+ * Esta clase se encarga de iniciar la interfaz gráfica del sistema
+ * utilizando Swing. Instancia la vista principal {@link MazeFrame}
+ * y la conecta con su correspondiente controlador {@link MazeController}.
+ * </p>
+ *
+ * <p>
+ * Se utiliza {@code SwingUtilities.invokeLater} para asegurar que
+ * la GUI se cree y maneje correctamente en el hilo de eventos de Swing.
+ * </p>
+ *
+ * @author TuNombre
+ * @version 1.0
+ */
 public class MazeAPP {
+    /**
+     * Método principal que lanza la aplicación.
+     *
+     * @param args los argumentos de línea de comandos (no utilizados)
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             MazeFrame vista = new MazeFrame();
